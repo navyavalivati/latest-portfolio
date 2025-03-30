f83e83849aed23a4681cbd8d329443a587348498
Navya Valivati <valivati.navya@gmail.com>
2025-03-30 01:44:38 -0500

    navbar changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,15 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ExperienceComponent } from './experience/experience.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'skills', component: SkillsComponent},
+  { path: 'experience', component: ExperienceComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' } // Wildcard route redirects to home
 ];
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  
 }
